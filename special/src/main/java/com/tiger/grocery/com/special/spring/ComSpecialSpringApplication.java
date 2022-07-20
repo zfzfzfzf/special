53d05a95b49e93b6230bde2d1dfbedf2e1e39dc1
zhangfeng <zhangfeng>
2022-07-20 08:11:31 +0800

    first commit

---
+++ special/src/main/java/com/tiger/grocery/com/special/spring/ComSpecialSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tiger.grocery.com.special.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComSpecialSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComSpecialSpringApplication.class, args);
+    }
+
+}
---
+++ special/src/test/java/com/tiger/grocery/com/special/spring/ComSpecialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiger.grocery.com.special.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComSpecialSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
